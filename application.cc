cced4e3df466e09ab881bb3dcae65b2b02ba3717
tbago <moonzalor@gmail.com>
2023-04-24 23:11:47 +0800

    Rollback glfw window hint

--- application.cc
+++ application.cc
@@ -53,6 +53,13 @@ int main(void) {
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "OpenGL Tutorial", NULL, NULL);
     if (!window) {
