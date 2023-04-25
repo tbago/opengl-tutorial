c7cb164edb1290aa764b6baae9893ea5450be6fc
tbago <moonzalor@gmail.com>
2023-04-25 15:38:50 +0800

    Implement lecture 11: Uniforms in OpenGL

--- application.cc
+++ application.cc
@@ -182,12 +182,26 @@ int main(void) {
 
     GLCall(glUseProgram(shader));
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    assert(location != -1);
+
+    float r = 0.0f;
+    float increment = 0.05f;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL));
+
+        if (r >= 1.0) {
+            increment = -0.05f;
+        } else if (r <= 0.0) {
+            increment = 0.05f;
+        }
+        r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
